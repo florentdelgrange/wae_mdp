a2695d7ef5b07c78c5e51aec1b9fdd26c62c0094
Florent Delgrange <florent.delgrange@gmail.com>
2023-02-28 14:28:10 +0100

    Add files via upload

--- inputs/pendulum.sh
+++ inputs/pendulum.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-python train.py --flagfile inputs/Pendulum-v0 --seed 20210621
-python train.py --flagfile inputs/Pendulum-v0 --seed 11111111
-python train.py --flagfile inputs/Pendulum-v0 --seed 22222222
-python train.py --flagfile inputs/Pendulum-v0 --seed 33333333
-python train.py --flagfile inputs/Pendulum-v0 --seed 44444444
-# Run using a uniform replay buffer to plot the histogram latent space comparison
-python train.py --flagfile inputs/Pendulum-v0 --seed 20210621 --prioritized_experience_replay=False
+python train.py --flagfile inputs/Pendulum-v1 --seed 11111
+python train.py --flagfile inputs/Pendulum-v1 --seed 22222
+python train.py --flagfile inputs/Pendulum-v1 --seed 33333
+python train.py --flagfile inputs/Pendulum-v1 --seed 44444
+python train.py --flagfile inputs/Pendulum-v1 --seed 55555
